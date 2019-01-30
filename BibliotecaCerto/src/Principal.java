96849f7ee147be724acd083594985b9dc2f1efce
igorromao <42851423+igorromao@users.noreply.github.com>
2019-01-30 10:55:41 -0200

    Add files via upload

---
+++ BibliotecaCerto/src/Principal.java
@@ -0,0 +1,29 @@
+
+import javax.swing.ImageIcon;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author igor
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BoasVindas puxar = new BoasVindas();
+        puxar.setVisible(true);
+        
+        
+        
+        
+    }
+ 
+    
+}
